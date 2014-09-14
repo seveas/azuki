96f1e652ec33552ab84246796266908708d3f23d
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-09-14 22:42:19 +0200

    Add a setup.py and release the first version

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "azuki",
+      version = "0.9",
+      author = "Dennis Kaarsemaker",
+      author_email = "dennis@kaarsemaker.net",
+      url = "http://github.com/seveas/azuki",
+      description = "Beanstalkd utilities for CLI and python",
+      package_dir = {'': 'lib'},
+      packages = ["azuki"],
+      scripts = ["bin/azuki"],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
+)
